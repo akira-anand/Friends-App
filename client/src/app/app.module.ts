741df388d3eeed8efc88f982dc34d9e74aaea39e
yash_personal_project <95654782+akira-anand@users.noreply.github.com>
2022-03-20 14:15:45 +0530

    form-group for register basic

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -50,6 +50,7 @@ import { PhotoEditComponent } from './members/photo-edit/photo-edit.component';
     HttpClientModule,
     BrowserAnimationsModule,
     FormsModule,
+    ReactiveFormsModule,
     SharedModule,
     NgxSpinnerModule
   ],
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,7 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
-import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -13,25 +12,45 @@ export class RegisterComponent implements OnInit {
   // @Input () usersFromHomeComponent: any;
   @Output() cancelRegister= new EventEmitter();
   model:any={};
+  registerForm:FormGroup;
   constructor(private accountService:AccountService,private toastr:ToastrService) { }
 
   ngOnInit(): void {
+    this.initializeForm();
   }
+  initializeForm(){
+    this.registerForm=new FormGroup({
+      username:new FormControl('',Validators.required),
+      password:new FormControl('',[Validators.required,Validators.minLength(4),Validators.maxLength(8)]),
+      confirmPassword:new FormControl('',[Validators.required,this.matchValues("password")]),
+    })
+    this.registerForm.controls.password.valueChanges.subscribe(()=>{
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    })
+  }
+  matchValues (matchTo: string): ValidatorFn{
+    return (control: AbstractControl) => {
+    return control?.value === control?.parent?.controls[matchTo].value
+    ? null: {isMatching: true}
+  }
+}
   register()
   {
+    console.log(this.registerForm.value);
 // console.log(this.usersFromHomeComponent);
-this.accountService.register(this.model).subscribe( response => {
-  console. log (response);
-  this.cancel();
-},
-  error => {
-  console.log(error.error);
-  this.toastr.error(error.error);
-  })
-}
-  cancel()
-  {
-this.cancelRegister.emit(false);
-  }
+// this.accountService.register(this.model).subscribe( response => {
+//   console. log (response);
+//   this.cancel();
+// },
+//   error => {
+//   console.log(error.error);
+//   this.toastr.error(error.error);
+//   })
+// }
+//   cancel()
+//   {
+// this.cancelRegister.emit(false);
+//   }
 
+}
 }
